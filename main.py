6f0df681f4f31e7d0f3a3b55c60a1dd5167a84e9
alexisneveu <alexis.nvu@gmail.com>
2024-01-31 16:47:23 +0100

    print hello world

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import random
 
+
 def main():
     # Generate a random number between 1 and 10
     random_number = random.randint(1, 10)
     print(f"The random number is: {random_number}")
+    print("Hello, World!")
+
 
 if __name__ == "__main__":
     main()
