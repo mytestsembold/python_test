a6479eb9de598dc482543801001a0140195807f5
alexisneveu <alexis.nvu@gmail.com>
2024-03-13 12:08:01 +0100

    hello

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ def main():
     random_number = random.randint(1, 10)
     print(f"The random number is: {random_number}")
 
-    # Check if the random number is even
-    if random_number % 2 == 0:
-        print("The random number is even.")
-    else:
-        print("The random number is odd.")
-
 
 
 if __name__ == "__main__":
