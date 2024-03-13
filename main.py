d2a95eac83327261642e295df4cba093292c70cd
alexisneveu <alexis.nvu@gmail.com>
2024-03-13 16:17:04 +0100

    HOTFIX:aaaaah

--- main.py
+++ main.py
@@ -13,11 +13,6 @@ def main():
     random_number = random.randint(1, 10)
     print(f"The random number is: {random_number}")
 
-    # Generate a random number between 1 and 10
-    random_number = random.randint(1, 10)
-    print(f"The random number is: {random_number}")
-
-
 
 
 if __name__ == "__main__":
